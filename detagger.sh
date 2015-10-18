066d5cd05159c34568948714def4fd420004a627
Remy <fevrer@localhost.localdomain>
2015-10-18 22:17:58 +0200

    amélioration des scripts detagger.sh et tagger.sh

--- detagger.sh
+++ detagger.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-#Script qui supprime un tag
-tag=$1;
-shift;
+#Script qui supprime un ou plusieur fichier dans des tags
+#Param: Les Tags d'abord, ensuite après --fichier, le nom des fichier a supprimer
+tabTag=();
+i=0;
 
-for j in $@
+while [ "$1" != "--fichier" ]
 do
-    rm $HOME/tags/$tag/$j
+    tabTag[$i]=$1
+    ((i++))
+    shift
 done
 
-rmdir --ignore-fail-on-non-empty $HOME/tags/$tag
+shift
+
+#Boucle pour supprimer un ou plusieurs liens physique d'un fichier en fonction d'un ou plusieurs Tags
+for fichier in $@
+do
+    for tag in ${tabTag[@]}
+    do
+	rm $HOME/tags/$tag/$fichier
+	rmdir --ignore-fail-on-non-empty $HOME/tags/$tag
+    done
+done
+
+
--- tagger.sh
+++ tagger.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#Script qui met un tag sur chaque fichier envoyé en paramétre et créer un lien dans /tag 
+#Script qui met un tag sur chaque fichier envoyé en paramétre et créer un lien dans /tag
+#Param: Les tags d'abord, ensuite après --chemin, le chemin des fichier pour créer un lien physique
 tabTag=();
 i=0;
 
@@ -8,7 +9,6 @@ while [ "$1" != "--chemin" ]
 do
     mkdir -p $HOME/tags/$1
     tabTag[$i]=$1
-    echo ${tabTag[$i]}
     ((i++))
     shift
 done
@@ -16,13 +16,12 @@ done
 shift
 
 #Boucle pour créer un lien physique d'un fichier dans chaque Tags donné par l'utilisateur
-for fichier in $@
+for cheminFichier in $@
 do
     for tag in ${tabTag[@]}
     do
-	ln -f $fichier $HOME/tags/$tag/
+	ln -f $cheminFichier $HOME/tags/$tag/
     done
-    shift
 done
 
 
